313d8b1f59215d58d06af5b40377c317de6a4bec
William Crownover <williamcrownover1@gmail.com>
2021-05-23 21:37:30 -0700

    Created first question object and tested new function loadQuestion

---
+++ Assets/js/questions.js
@@ -0,0 +1,13 @@
+//DECLARING QUESTION OBJECTS
+var questions = [
+    {
+    question: "Which is the correct form to output a message to the dev tools.",
+    correct: "console.log()",
+    choices: [
+        "console.log()",
+        "log.console()",
+        "displayContent()",
+        "whatIsThis()"
+        ]
+    }
+];
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,6 +5,7 @@ var timerEl = document.querySelector("#timerValue");
 var startScreenEl = document.querySelector("#startScreen");
 var startQuizBtn = document.querySelector("#startQuizButton");
 var questionScreenEl = document.querySelector("#questionScreen");
+var questionEl = document.querySelector("#theQuestion");
 var correctEl = document.querySelector(".correct");
 var wrongEl = document.querySelector(".wrong");
 var finishedScreenEl = document.querySelector("#finishScreen");
@@ -33,11 +34,19 @@ function startTimer() {
     }, 1000);
 }
 
+function loadQuestion() {
+    questionEl.textContent = questions[0].question;
+    for(var i = 0; i < questions[0].choices.length; i++) {
+        questionScreenEl.children[2 + i*2].textContent = questions[0].choices[i];
+    }
+}
+
 function startGame() {
     startScreenEl.classList.add("hidden");
     questionScreenEl.classList.remove("hidden");
     
     startTimer();
+    loadQuestion();
 }
 
 startQuizBtn.addEventListener("click", function() {
