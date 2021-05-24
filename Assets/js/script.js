e92ecd915479fa91f5bbadd1e035772ca6be70a4
William Crownover <williamcrownover1@gmail.com>
2021-05-23 20:47:21 -0700

    Developed the startTimer function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -12,7 +12,28 @@ var correctScoreSpan = document.querySelector("#answeredCorrectScore");
 var timerScoreSpan = document.querySelector("#finalScoreTime");
 
 correctAnswersEL.textContent = 0;
-timerEl.textContent = 100;
+timerEl.textContent = 5;
 correctScoreSpan.textContent = correctAnswersEL.textContent;
 timerScoreSpan.textContent = timerEl.textContent;
 
+function startTimer() {
+    var timerInterval = setInterval(function() {
+        timerEl.textContent -= 1;
+
+        if(timerEl.textContent == 0) {
+            clearInterval(timerInterval);
+            endGame();
+        }
+
+    }, 1000);
+}
+
+function startGame() {
+    startScreenEl.classList.add("hidden");
+    questionScreenEl.classList.remove("hidden");
+    startTimer();
+}
+
+startQuizBtn.addEventListener("click", function() {
+    startGame();
+} );
