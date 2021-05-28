abec97a584bb056da25027e8f9f72dc9fcc7ab8c
William Crownover <williamcrownover1@gmail.com>
2021-05-28 01:54:47 -0700

    Added comments to explain functions. Adjusted the setTimeout on the correct wrong display to work if answered in rapid succesion.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,5 @@
-// GLOBL VARIABLES
-//Element Variables
+// GLOBAL VARIABLES
+// Element Variables
 var correctAnswersEL = document.querySelector("#correctAnswers");
 var timerEl = document.querySelector("#timerValue");
 var startScreenEl = document.querySelector("#startScreen");
@@ -15,20 +15,21 @@ var timerScoreSpan = document.querySelector("#finalScoreTime");
 var initalsFormEl = document.querySelector("#initalsForm");
 var initalsEl = document.querySelector("#initials");
 
+// Tracking Variables
 var index;
-var correctAnswers = 0;
-var timeLeft = 10*questions.length;
+var correctAnswers;
+var timeLeft;
 var timerInterval;
-
-correctAnswersEL.textContent = correctAnswers;
-timerEl.textContent = timeLeft;
+var correctWrongTimeout;
 
 // --------------------------------------------------------------------------------
 // DECLARED FUNCTIONS
+// Updates the timer value to immediate value
 function updateTimerValue() {
     timerEl.textContent = timeLeft;
 }
 
+// Stops the timer and switches content to finished screen with score
 function endGame() {
     clearInterval(timerInterval);
     updateTimerValue();
@@ -45,9 +46,11 @@ function endGame() {
     timerScoreSpan.textContent = timeLeft;
 }
 
+// Starts the timer countdown interval and tracks game end conditions
 function startTimer() {
     timerInterval = setInterval(function() {
         timeLeft -= 1;
+
         updateTimerValue();
 
         if(timeLeft <= 0 || index === questions.length) {
@@ -57,7 +60,7 @@ function startTimer() {
     }, 1000);
 }
 
-// Fisher-Yates shuffle
+// Fisher-Yates shuffle: shuffles the question order and answer order
 function shuffleArray(array) {
     for( var i = array.length - 1; i > 0; i-- ) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -68,10 +71,10 @@ function shuffleArray(array) {
     return array;
 }
 
+// Loads a question object's properties to the question section of html
 function loadQuestion() {
     questionEl.textContent = questions[index].question;
     questionCoutdownEl.textContent = questions.length - index;
-
     questions[index].choices = shuffleArray(questions[index].choices);
 
     for(var i = 0; i < questions[index].choices.length; i++) {
@@ -80,6 +83,7 @@ function loadQuestion() {
     }
 }
 
+// Activated on button click, switches to question content and initializes variable values
 function startGame() {
     startScreenEl.classList.add("hidden");
     questionScreenEl.classList.remove("hidden");
@@ -93,44 +97,54 @@ function startGame() {
     loadQuestion();
 }
 
+// Hides the "correct" or "wrong" elements
 function hideCorrectWrong() {
-    setTimeout(function() {
-        correctEl.classList.add("hidden");
-        wrongEl.classList.add("hidden");
-    }, 1000);
+    correctEl.classList.add("hidden");
+    wrongEl.classList.add("hidden");
 }
 
+// Keeps track of how long the "correct" or "wrong" elements have been displayed
+function correctWrongTimer() {
+    correctWrongTimeout = setTimeout(hideCorrectWrong, 1000);
+}
+
+// Updates index to load next question or end the game once all questions have been answered
 function nextQuestion() {
     index++;
-    
-    updateTimerValue();
 
     if(index === questions.length) {
+        correctWrongTimer();
         endGame();
         return
     }
 
-    hideCorrectWrong();
+    correctWrongTimer();
     loadQuestion();
 }
 
+// Evaluates if the answer button value was correct or wrong and updates counters in response
 function checkAnswer(event) {
     if(event.target.type === "submit") {
         var answer = event.target.value;
+
+        clearTimeout(correctWrongTimeout);
+        hideCorrectWrong()
+
         if(answer === questions[index].correct) {
             correctAnswers++;
             correctAnswersEL.textContent = correctAnswers;
             correctEl.classList.remove("hidden");
             nextQuestion();
-            
         } else {
             timeLeft -= 10;
+            updateTimerValue();
             wrongEl.classList.remove("hidden");
             nextQuestion();
         }
     }
 }
 
+// Saves the players score from a form and stores the data as a JSON object array locally
 function saveScore(event) {
     event.preventDefault();
 
