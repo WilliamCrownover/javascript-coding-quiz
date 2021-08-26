3faeee8e98ff5026e399d497b7601d041e8014f9
William Crownover <williamcrownover1@gmail.com>
2021-08-26 04:19:11 -0700

    Formatting with eslint and stylelint

--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -1,123 +1,124 @@
-//DECLARING QUESTION OBJECTS ARRAY
+// DECLARING QUESTION OBJECTS ARRAY
+// eslint-disable-next-line no-unused-vars
 var questions = [
-    {
-    question: "Which is the correct form to output a message to the dev tools?",
-    correct: "console.log()",
-    choices: [
-        "console.log()",
-        "log.console()",
-        "displayContent()",
-        "showMeWhatThisIs()"
-        ]
-    },
-    {
-    question: "What element do we use to embed JavaScript into our HTML?",
-    correct: "<script>",
-    choices: [
-        "<script>",
-        "<header>",
-        "<link>",
-        "<javascript>"
-        ]
-    },
-    {
-    question: "Which primitive data type returns a true or false value?",
-    correct: "boolean",
-    choices: [
-        "boolean",
-        "string",
-        "number",
-        "ifElse"
-        ]
-    },
-    {
-    question: "To assign a value to a variable which operator should we use?",
-    correct: "=",
-    choices: [
-        "=",
-        "==",
-        "===",
-        "equals"
-        ]
-    },
-    {
-    question: "To compare if two values are equal in value and data type which operator should we use?",
-    correct: "===",
-    choices: [
-        "===",
-        "==",
-        "=",
-        "equals"
-        ]
-    },
-    {
-    question: "Which set of parentheses are used to declare or store values in an array?",
-    correct: "[ ] Square",
-    choices: [
-        "[ ] Square",
-        "( ) Round",
-        "{ } Curly",
-        "< > Angle"
-        ]
-    },
-    {
-    question: "Which of the following is NOT an expression used to define the parameters of a 'for' loop?",
-    correct: "Functional",
-    choices: [
-        "Functional",
-        "Declarative",
-        "Conditional",
-        "Incremental"
-        ]
-    },
-    {
-    question: "When we want to _____ a declared function we put () after the name.",
-    correct: "Call",
-    choices: [
-        "Call",
-        "Reference",
-        "Summon",
-        "Trigger"
-        ]
-    },
-    {
-    question: "If we want a variable to be accessible to any part of our code we declare it _____.",
-    correct: "Globally",
-    choices: [
-        "Globally",
-        "Universally",
-        "Locally",
-        "Openly"
-        ]
-    },
-    {
-    question: "Which keyword is NOT used to declare a variable?",
-    correct: "store",
-    choices: [
-        "store",
-        "var",
-        "let",
-        "const"
-        ]
-    },
-    {
-    question: "What property of an array returns how many items are stored in it?",
-    correct: ".length",
-    choices: [
-        ".length",
-        ".count",
-        ".sum",
-        ".total"
-        ]
-    },
-    {
-    question: "The value of _____ is determined by where it is used and always refers to an object.",
-    correct: "this",
-    choices: [
-        "this",
-        "that",
-        "here",
-        "aforementioned"
-        ]
-    }
+	{
+		question: 'Which is the correct form to output a message to the dev tools?',
+		correct: 'console.log()',
+		choices: [
+			'console.log()',
+			'log.console()',
+			'displayContent()',
+			'showMeWhatThisIs()'
+		]
+	},
+	{
+		question: 'What element do we use to embed JavaScript into our HTML?',
+		correct: '<script>',
+		choices: [
+			'<script>',
+			'<header>',
+			'<link>',
+			'<javascript>'
+		]
+	},
+	{
+		question: 'Which primitive data type returns a true or false value?',
+		correct: 'boolean',
+		choices: [
+			'boolean',
+			'string',
+			'number',
+			'ifElse'
+		]
+	},
+	{
+		question: 'To assign a value to a variable which operator should we use?',
+		correct: '=',
+		choices: [
+			'=',
+			'==',
+			'===',
+			'equals'
+		]
+	},
+	{
+		question: 'To compare if two values are equal in value and data type which operator should we use?',
+		correct: '===',
+		choices: [
+			'===',
+			'==',
+			'=',
+			'equals'
+		]
+	},
+	{
+		question: 'Which set of parentheses are used to declare or store values in an array?',
+		correct: '[ ] Square',
+		choices: [
+			'[ ] Square',
+			'( ) Round',
+			'{ } Curly',
+			'< > Angle'
+		]
+	},
+	{
+		question: 'Which of the following is NOT an expression used to define the parameters of a \'for\' loop?',
+		correct: 'Functional',
+		choices: [
+			'Functional',
+			'Declarative',
+			'Conditional',
+			'Incremental'
+		]
+	},
+	{
+		question: 'When we want to _____ a declared function we put () after the name.',
+		correct: 'Call',
+		choices: [
+			'Call',
+			'Reference',
+			'Summon',
+			'Trigger'
+		]
+	},
+	{
+		question: 'If we want a variable to be accessible to any part of our code we declare it _____.',
+		correct: 'Globally',
+		choices: [
+			'Globally',
+			'Universally',
+			'Locally',
+			'Openly'
+		]
+	},
+	{
+		question: 'Which keyword is NOT used to declare a variable?',
+		correct: 'store',
+		choices: [
+			'store',
+			'var',
+			'let',
+			'const'
+		]
+	},
+	{
+		question: 'What property of an array returns how many items are stored in it?',
+		correct: '.length',
+		choices: [
+			'.length',
+			'.count',
+			'.sum',
+			'.total'
+		]
+	},
+	{
+		question: 'The value of _____ is determined by where it is used and always refers to an object.',
+		correct: 'this',
+		choices: [
+			'this',
+			'that',
+			'here',
+			'aforementioned'
+		]
+	}
 ];
